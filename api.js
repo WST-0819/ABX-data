225bd2d4aec9fe994b4abf56fd9e9a28362b013e
WST-0819 <weist0819@gmail.com>
2026-06-29 16:07:19 +0800

    Update api.js

--- api.js
+++ api.js
@@ -2,36 +2,24 @@
 export const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdOy22a2Li6l51RSKeA29mpBcSqTuPlDUuePw2QLRmerWJL--xvYX1CL9V47r-csEFIqjWveW8N2AT/pub?gid=1596306221&single=true&output=csv";
 
 export async function fetchAntibioticData() {
-  const cachedData = sessionStorage.getItem('antibiotic_data');
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
-
-  // 當快取不存在時，進行實際請求
-  try {
-    const response = await fetch(GOOGLE_SHEET_CSV_URL);
-    if (!response.ok) {
-      throw new Error(`Network response was not ok (${response.status})`);
-    }
-    const csvText = await response.text();
-
-    // 使用 PapaParse 解析
-    const results = Papa.parse(csvText, {
-      header: true,
-      skipEmptyLines: true
+  return new Promise((resolve, reject) => {
+    Papa.parse(GOOGLE_SHEET_CSV_URL, {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: function(results) {
+            if (results.data && results.data.length > 0) {
+                sessionStorage.setItem('antibiotic_data', JSON.stringify(results.data));
+                resolve(results.data);
+            } else {
+                reject('無效的資料格式');
+            }
+        },
+        error: function(err) {
+            reject(err);
+        }
     });
-
-    if (!results || !results.data) {
-      throw new Error('Invalid CSV parse result');
-    }
-
-    const data = results.data;
-    sessionStorage.setItem('antibiotic_data', JSON.stringify(data));
-    return data;
-  } catch (error) {
-    console.error("CSV 獲取失敗:", error);
-    throw error;
-  }
+  });
 }
 
         Papa.parse(GOOGLE_SHEET_CSV_URL, {
